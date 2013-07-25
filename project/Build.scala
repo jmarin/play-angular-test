604b88f64c2dc19705539b3a6baa9c3da778f302
jmarin <juan.marin.otero@gmail.com>
2013-07-25 14:09:10 -0400

    Add play-angular configuration

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+import com.tuplejump.sbt.yeoman.Yeoman
 
 object ApplicationBuild extends Build {
 
@@ -15,7 +16,8 @@ object ApplicationBuild extends Build {
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    Yeoman.yeomanSettings : _*
   )
 
 }
