4472c04dcee4f90c29204ea60201b8ff85889b48
jmarin <juan.marin.otero@gmail.com>
2013-07-30 17:38:35 -0400

    Embed Scala template with AngularJS

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,21 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
+import play.api.libs.json._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+
+  def template = Action {
+  	Ok(views.html.template("Repo template", Repository("repo")))
+  }
+
+  def api = Action {
+  	Ok(Json.obj("api" -> "GeoEnt"))
+  }
 }
--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
 angular.module('uiApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
+
+    $http.get('/template').success(function(data) {
+      //console.log(data);
+      $scope.template = data;
+    });
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+
+    //console.log(MyService.data);
+
   });
